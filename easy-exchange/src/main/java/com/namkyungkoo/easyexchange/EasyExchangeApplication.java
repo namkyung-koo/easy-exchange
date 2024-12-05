ec122ff6f574ddc34bb79724afbcf82602d109cf
Namkyung Koo <united94@naver.com>
2024-12-05 23:02:28 +0900

    feat: setup easy-exchange project

---
+++ easy-exchange/src/main/java/com/namkyungkoo/easyexchange/EasyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.namkyungkoo.easyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyExchangeApplication.class, args);
+	}
+
+}
---
+++ easy-exchange/src/test/java/com/namkyungkoo/easyexchange/EasyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namkyungkoo.easyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
